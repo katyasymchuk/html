e630c48b34a5e4caa8b4d91ec49f98154970fa21
katyasymchuk <72002976+katyasymchuk@users.noreply.github.com>
2022-07-25 13:09:27 +0300

    js1

---
+++ jslesson1/main.js
@@ -0,0 +1,77 @@
+let stra = 'hello';
+console.log(stra);
+let strb = 'owu';
+console.log(strb);
+let strc = 'com';
+console.log(strc);
+let strd = 'ua';
+console.log(strd);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+let i = 3.14;
+console.log(i);
+let j = 2.7;
+console.log(j);
+let k = 16;
+console.log(k);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2);
+// alert(stra);
+// alert(strb);
+// alert(strc);
+// alert(strd);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+// alert(j);
+// alert(k);
+// alert(bool1);
+// alert(bool2);
+document.write(stra, strb, strc, strd, e, f, g, h, i, j, k, bool1, bool2);
+let str1 = 'Kate';
+let str2 = 'Yuryivna';
+let str3 = 'Symchuk';
+let str4 = str1 + str2 + str3;
+console.log(str4);
+let words = `${str1} ${str2} ${str3}`;
+console.log(words);
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+let name = prompt("What is your name?");
+
+alert(`Your name ${name} !`);
+
+let middleName = prompt("What is your middle name?");
+
+alert(`You middle name ${middleName} !`);
+
+let age = prompt("How old are you??");
+
+alert(`You last name ${age} !`);
+
+console.log(name);
+console.log(middleName);
+console.log(age);
+
+
+
+
+
+
+
+
